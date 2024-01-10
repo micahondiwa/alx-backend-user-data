f9e887f91753b45242d8dfe5ca1172e817a1fb2d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-10 10:32:47 +0300

    filtered logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
