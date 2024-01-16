4064ea5bb1e2b180674b67d59c556414cbe65a84
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-16 19:15:28 +0300

    fx

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,7 +21,7 @@ class BasicAuth(Auth):
             - Base64 part of the Authorization header for basic authentication
         """
         if authorization_header:
-            if type(authorization_header) != str:
+            if type(authorization_header) is not str:
                 return None
             pattern = r"Basic (?P<token>.+)"
             if re.fullmatch(pattern, authorization_header.strip()):
@@ -39,7 +39,7 @@ class BasicAuth(Auth):
             - Decoded value of a Base64 string base64_authorization_header
         """
         if base64_authorization_header:
-            if type(base64_authorization_header) != str:
+            if type(base64_authorization_header) is not str:
                 return None
             try:
                 res = b64decode(base64_authorization_header, validate=True)
@@ -56,7 +56,7 @@ class BasicAuth(Auth):
             - user email and password from the Base64 decoded value
         """
         if decoded_base64_authorization_header:
-            if type(decoded_base64_authorization_header) != str:
+            if type(decoded_base64_authorization_header) is not str:
                 return None, None
             if ":" in decoded_base64_authorization_header:
                 email = decoded_base64_authorization_header.split(":")[0]
@@ -72,7 +72,7 @@ class BasicAuth(Auth):
         Return:
             - User instance based on his email and password
         """
-        if type(user_email) == str and type(user_pwd) == str:
+        if type(user_email) == str and type(user_pwd) is str:
             try:
                 users = User.search({"email": user_email})
                 if not users or users == []:
