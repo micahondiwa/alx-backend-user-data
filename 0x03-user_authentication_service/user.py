e04b3e35a6b6b6490a4661c4c34225058b30e51d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-25 10:58:28 +0300

    FIx

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -16,7 +16,7 @@ class User(Base):
 
     __tablename__ = "users"
 
-    id = Column(primary_key=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(string(250))
